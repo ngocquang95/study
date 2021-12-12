e9186bab24fa9f10427be093341060f609139b46
Ngoc Quang <ngocquang12395@gmail.com>
2021-12-13 00:09:37 +0700

    develop

---
+++ hello-world/src/main/java/basiclearning/HelloWorld.java
@@ -0,0 +1,14 @@
+package basiclearning;
+
+public class HelloWorld {
+    private String property1;
+    private String property2;
+    private String property3;
+    private String property4;
+    private String property5;
+    private String property6;
+    private String property7;
+    private String property8;
+    private String property9;
+    private String property10;
+}
