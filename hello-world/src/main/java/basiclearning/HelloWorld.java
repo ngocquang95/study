8ecdec7daa0e24ff6277697f149d42ac4dabb602
Ngoc Quang <ngocquang12395@gmail.com>
2021-12-13 00:40:43 +0700

    feature/merge/02

---
+++ hello-world/src/main/java/basiclearning/HelloWorld.java
@@ -0,0 +1,38 @@
+package basiclearning;
+
+public class HelloWorld {
+    private String property1;
+    private String property2;
+    private String property3;
+    private String property4;
+    private String property5;
+    private String property6;
+    private String property7;
+    private String property8;
+    private String property9;
+    private String property10;
+
+    public String getProperty1() {
+        return property1;
+    }
+
+    public void setProperty1(String property1) {
+        this.property1 = property1;
+    }
+
+    public String getProperty2() {
+        return property2;
+    }
+
+    public void setProperty2(String property2) {
+        this.property2 = property2;
+    }
+
+    public String getProperty3() {
+        return property3;
+    }
+
+    public void setProperty3(String property3) {
+        this.property3 = property3;
+    }
+}
