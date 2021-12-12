49d9c4b0453d20097939cea5e6b6c525162f474c
Ngoc Quang <ngocquang12395@gmail.com>
2021-12-12 23:55:32 +0700

    feature/1

---
+++ hello-world/src/main/java/basiclearning/HelloWorld.java
@@ -0,0 +1,22 @@
+package basiclearning;
+
+public class HelloWorld {
+    private String property1;
+    private String property2;
+    private String property3;
+    private String property4;
+    private String property5;
+    private String property6;
+    private String property7;
+    private String property8;
+    private String property9;
+    private String property10;
+
+    public String getProperty1() {
+        return property1;
+    }
+
+    public void setProperty1(String property1) {
+        property1 = property1;
+    }
+}
